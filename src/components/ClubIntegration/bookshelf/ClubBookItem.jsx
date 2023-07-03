2c6ea460db6a7a1e3e1aff6f41f39d6d4793ee90
mpmcauliffe <mcauliffe99@gmail.com>
2023-07-02 23:05:45 -0400

    COMPONENTS: create clubIntegration/bookshelf/ ClubBookItem & ClubShelf

---
+++ src/components/ClubIntegration/bookshelf/ClubBookItem.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { useParams, } from 'react-router-dom'
+import { ClubBookShelf, } from '../../bookShelf/Books.comp'
+import { AppButton, } from '../..'
+import { addBookToClub, removeBookFromClub, } from '../../../redux/actions/bookActions'
+
+
+const ClubBookItem_proto = ({ addBookToClub, removeBookFromClub,  
+    book, isUserBookshelf, isCheifAdmin, }) => {
+    
+    const { clubId } = useParams()
+    const { bookId, title, infoLink, image, authors } = book
+    
+
+    return (
+        <ClubBookShelf showButton={isUserBookshelf || isCheifAdmin ? true : false}>
+            <img src={`${image}`} alt='cover' className='cover' />
+            <div>
+                <p className='title'><strong>{title}</strong></p>
+                <section>
+                    <p>{authors.map(author => <span key={author}>{author}&nbsp;&nbsp;&nbsp;</span>)}</p>
+                    
+                    <div style={{ display: 'inline-block' }}>
+                        {isUserBookshelf 
+                            && <AppButton
+                            name={bookId}
+                            title={title}
+                            alertButton={false}
+                            onClick={() => addBookToClub(clubId, book)}
+                            style={{ marginRight: '1rem', float: 'right' }}>
+                                Add</AppButton>}
+                            
+                        {isCheifAdmin && !isUserBookshelf 
+                            ? <AppButton
+                            name={bookId}
+                            title={title}
+                            alertButton={true}
+                            onClick={() => removeBookFromClub(clubId, book)}
+                            style={{ marginRight: '1rem', float: 'right' }}>
+                                Remove</AppButton>
+                            : null }
+                    </div>
+                    {/*  */}
+                </section> 
+                {!isUserBookshelf
+                    && <div className='link'>
+                        <a href={`${infoLink}`}>
+                            <p style={{ color: '#000' }}>view book on google</p>
+                        </a>
+                    </div>
+                }
+            </div>
+        </ClubBookShelf>
+    )
+
+    // if (isUserBookshelf) { return bookItem }
+    
+    // return (
+    //     <a href={`${infoLink}`}>
+    //         {bookItem}
+    //     </a>    
+    // )
+}
+
+ClubBookItem_proto.propTypes = {
+    addBookToClub: PropTypes.func.isRequired,
+    removeBookFromClub: PropTypes.func.isRequired,
+    book: PropTypes.object.isRequired,
+    isUserBookshelf: PropTypes.bool.isRequired,
+    isCheifAdmin: PropTypes.bool.isRequired,
+}
+
+const mapStateToProps = state => ({
+    addBookToClub: state.books.addBookToClub,
+    removeBookFromClub: state.books.removeBookFromClub,
+    isCheifAdmin: state.clubs.isCheifAdmin,
+})
+
+const ClubBookItem = connect(mapStateToProps, { addBookToClub, removeBookFromClub, })(ClubBookItem_proto)
+export { ClubBookItem }
---
+++ src/components/ClubIntegration/bookshelf/ClubShelf.jsx
@@ -0,0 +1,98 @@
+import React, { Fragment, useState, useEffect, useReducer, } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import Simplebar from 'simplebar-react'
+import { ClubBookItem, } from './ClubBookItem'
+import { BasicTrigger, Buffer, EmptyNotification, } from '../..'
+import { getClubBooks, getBooks, } from '../../../redux/actions/bookActions'
+import 'simplebar/dist/simplebar.min.css'
+
+
+const clubBookshelfReducer = (state, action) => {
+    switch (action.type) {
+        case 'TOGGLE_BOOKSHELF':
+            return {
+                ...state,
+                bookshelfTitle: action.payload ? 'Your Bookshelf' : 'Club Bookshelf',
+                toggleTriggerText: action.payload ? 'Click here to VIEW club bookshelf' : 'Click here to ADD books from your bookshelf',
+                bookshelfMessage: action.payload ? 'You don\'t have any books in your library' : 'There aren\'t any books in this library',
+            }
+        default: 
+            return state
+    }
+}
+
+const initialState = { bookshelfTitle: '', toggleTriggerText: '', bookshelfMessage: '', }
+
+
+const ClubShelf_proto = ({ getClubBooks, getBooks, clubBooks, myBooks, isUserAMember, }) => {
+
+    const [state, dispatch] = useReducer(clubBookshelfReducer, initialState)
+
+    const [showUserBookshelf, setShowUserBookshelf]     = useState(false)
+    const [booksOnDisplay, setBooksOnDisplay]           = useState([ ])
+
+    const { bookshelfTitle, bookshelfMessage, toggleTriggerText, } = state
+        
+    useEffect(() => {
+        showUserBookshelf ? setBooksOnDisplay(myBooks) : setBooksOnDisplay(clubBooks)
+        dispatch({ type: 'TOGGLE_BOOKSHELF', payload: showUserBookshelf }) 
+
+    }, [myBooks, clubBooks, showUserBookshelf])
+
+
+    return (
+        <div>
+            <h3>{bookshelfTitle}</h3>
+            <Buffer thickness={7} />
+            
+            {isUserAMember 
+                && <Fragment>
+                    <BasicTrigger 
+                        onClick={() => setShowUserBookshelf(!showUserBookshelf)}>
+                        {toggleTriggerText}
+                    </BasicTrigger>
+                    {/* {showUserBookshelf && <p style={{ position: 'absolute', }}>Click 'Add' button to add a book to this club's library</p>} */}
+                    
+                    <Buffer thickness={3} />
+                </Fragment>}
+            
+            {Array.isArray(booksOnDisplay) && booksOnDisplay.length > 0 
+                ?  (<Simplebar style={{ height: '600px' }}>
+                       {booksOnDisplay.map(book => (
+                            <div key={book.bookId}>
+                                <ClubBookItem 
+                                    book={book}
+                                    isUserBookshelf={showUserBookshelf} />
+                                <Buffer thickness={.5} />
+                            </div>))}
+                    </Simplebar> 
+                ) : (
+                    <EmptyNotification 
+                        linkTo={''}
+                        linkMessage={''}
+                        preMessage={bookshelfMessage} />
+                )
+            }
+        </div>
+    )
+}
+
+ClubShelf_proto.propTypes = {
+    clubBooks: PropTypes.array.isRequired,
+    getClubBooks: PropTypes.func.isRequired,
+    myBooks: PropTypes.array.isRequired,
+    getBooks: PropTypes.func.isRequired,
+    isUserAMember: PropTypes.bool.isRequired,
+}
+
+const mapStateToProps = state => ({
+    getClubBooks: state.books.getClubBooks,
+    clubBooks: state.books.clubBooks,
+    getBooks: state.books.getBooks,
+    myBooks: state.books.myBooks,
+    isUserAMember: state.clubs.isUserAMember,
+})
+
+const ClubShelf = connect(mapStateToProps, { getClubBooks, getBooks, })(ClubShelf_proto)
+export { ClubShelf }
