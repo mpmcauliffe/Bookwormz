264e2639854c2bd9a74c632b4b2d8ae41b554929
mpmcauliffe <mcauliffe99@gmail.com>
2023-07-19 23:50:08 -0400

    FRONTEND: add redux/reducers/ bookReducer & clubReducer

---
+++ frontend/src/redux/reducers/bookReducer.js
@@ -0,0 +1,122 @@
+import { GET_MY_BOOKS, SEARCH_BOOKS, ADD_BOOK_TO_PROFILE, 
+    REMOVE_BOOK_FROM_LIBRARY, GET_CLUB_BOOKS, ADD_BOOK_TO_CLUB,
+    MESSAGE, BOOK_ERROR, CLEAR_BOOK_MESSAGE, SET_LOADING,  } from '../types'
+
+//M.toast({ html: `${bookMessage}`, classes: 'green darken-3 rounded', displayLength: 5000 })
+const initialState = {
+    bookSearchQuery: '',
+    myBooks: [ ],
+    clubBooks: [ ],
+    bookResults: [ ],
+    //bookAddedToClub: null,
+    isLoading: false,
+    shelfHeight: 100,
+    bookMessage: {
+        message: '',
+        style: '',
+        timeDisplay: null,
+    },
+    // myClubs: [ ],
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case GET_MY_BOOKS:
+            // console.log(action.payload)
+            return {    
+                ...state,
+                myBooks: [...action.payload],
+                isLoading: false,
+                // myClubs: [...action.payload.userDashboard.myClubs],
+            }
+
+        case SEARCH_BOOKS: 
+            return {
+                ...state,
+                bookResults: [...action.payload.books],
+                // UNCOMMENT FOR SAMPLE TEST bookResults: [...Array(7)].map((_, i) => action.payload.books), 
+                bookSearchQuery: action.payload.searchString,
+                isLoading: false,
+            }
+        
+        case ADD_BOOK_TO_PROFILE:
+            return {
+                ...state,
+                myBooks: [...state.myBooks, action.payload.book],
+                bookMessage: {
+                    message: action.payload.message,
+                    style: 'green darken-3 rounded',
+                    timeDisplay: 5000,
+                },
+            }
+
+        case REMOVE_BOOK_FROM_LIBRARY:
+            return {
+                ...state,
+                myBooks: state.myBooks.filter(book => book.bookId !== action.payload.book),
+                bookMessage: {
+                    message: action.payload.message,
+                    style: 'green darken-3 rounded',
+                    timeDisplay: 5000,
+                },
+            }
+
+        case GET_CLUB_BOOKS:
+            return {
+                ...state,
+                clubBooks: [...action.payload]
+            }
+
+        case ADD_BOOK_TO_CLUB:
+            return {
+                ...state,
+                clubBooks: [action.payload.book, ...state.clubBooks],
+                bookMessage: {
+                    message: action.payload.message,
+                    style: 'green darken-3 rounded',
+                    timeDisplay: 5000
+                }
+            }
+
+        case MESSAGE:
+            return {
+                ...state,
+                bookMessage: {
+                    message: action.payload.message,
+                    style: action.payload.style,
+                    timeDisplay: 6000,
+                },
+            }
+
+        case BOOK_ERROR:
+            // console.log(action.payload)
+            return {
+                ...state,
+                bookMessage: {
+                    message: action.payload,
+                    style: 'red accent-4 rounded',
+                    timeDisplay: 5000,
+                },
+            }
+
+        case CLEAR_BOOK_MESSAGE:
+            return {
+                ...state,
+                bookMessage: {
+                    message: '',
+                    style: '',
+                    timeDisplay: null,
+                },
+            }
+
+        case SET_LOADING:
+            return {
+                ...state,
+                isLoading: true,
+            }
+
+        default:
+            return state
+    }
+} 
---
+++ frontend/src/redux/reducers/clubReducer.js
@@ -0,0 +1,134 @@
+import { GET_CLUB, GET_MULTIPLE_CLUBS, GET_MY_CLUBS, CREATE_CLUB, JOIN_CLUB, LEAVE_CLUB, 
+    SEARCH_CLUBS, CLEAR_SEARCH_CLUBS, RESET_CLUB, CLUB_MESSAGE, CLEAR_CLUB_MESSAGE, } from '../types'
+
+
+const initialState = {
+    availableClubs: [ ],
+    myClubs: [ ],
+    clubStore: [ ],
+    currentClub: null,
+    clubId: null,
+    clubSearchQuery: '',
+    isUserAMember: false,
+    isCheifAdmin: false,
+    clubMessage: {
+        message: '',
+        style: '',
+        timeDisplay: null,
+    }
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = initialState, action) => {
+    switch(action.type) {
+
+        case GET_CLUB:
+            // console.log(action.payload.isUserChiefAdmin)
+            return {
+                ...state,
+                currentClub: { ...action.payload._doc },
+                isUserAMember: action.payload.isClubMember,
+                isCheifAdmin: action.payload.isUserChiefAdmin,
+            }
+
+        case GET_MULTIPLE_CLUBS:
+            return {
+                ...state,
+                availableClubs: [...action.payload],
+                clubStore: [...action.payload],
+            }
+
+        case GET_MY_CLUBS:
+            // console.log(action.payload); 
+            return {
+                ...state,
+                myClubs: Array.isArray(action.payload) ? [...action.payload] : action.payload,
+                clubStore: Array.isArray(action.payload) ? [...action.payload] : action.payload,
+            }
+
+        case CREATE_CLUB:
+            // console.log(action.payload)
+            return {
+                ...state,
+                availableClubs: [ ...state.availableClubs, action.payload.club._doc ],
+                clubStore: [ ...state.availableClubs, action.payload.club._doc ],
+                currentClub: { ...action.payload.club._doc },
+                isUserAMember: action.payload.club.isClubMember,
+                clubMessage: {
+                    message: action.payload.message,
+                    style: action.payload.style,
+                    timeDisplay: action.payload.timeDisplay,
+                },
+            }
+
+        case JOIN_CLUB:
+            console.log(action.payload.isClubMember);
+            return {
+                ...state,
+                isUserAMember: action.payload.isClubMember,
+                clubMessage: {
+                    message: action.payload.message,
+                    style: 'green darken-3 rounded',
+                    timeDisplay: 5000,
+                },
+            }
+
+        case LEAVE_CLUB:
+            return {
+                ...state,
+                isUserAMember: action.payload.isClubMember,
+                clubMessage: {
+                    message: action.payload.message,
+                    style: 'red accent-4 rounded', 
+                    timeDisplay: 5000,
+                },
+            }
+
+        case SEARCH_CLUBS:
+            return {
+                ...state,
+                // clubStore: state.availableClubs,
+                availableClubs: state.clubStore.filter(club => club.clubName.toLowerCase().includes(action.payload.toLowerCase()))
+            }
+
+        case CLEAR_SEARCH_CLUBS:
+            return {
+                ...state,
+                availableClubs: state.clubStore,
+                // clubStore: [ ],
+            }
+
+        case RESET_CLUB:
+            return {
+                ...state,
+                currentClub: null,
+                isUserAMember: false,
+                isCheifAdmin: false,
+                // clubBooks: [ ],
+            }
+
+        case CLUB_MESSAGE:
+            return {
+                ...state,
+                clubMessage: {
+                    message: action.payload.message,
+                    style: action.payload.style,
+                    timeDisplay: action.payload.timeDisplay,
+                },
+            }
+
+        case CLEAR_CLUB_MESSAGE:
+            return {
+                ...state,
+                clubMessage: {
+                    message: '',
+                    style: '',
+                    timeDisplay: null,
+                },
+            }
+
+        default:
+            return state
+    }
+} 
+
