a8b485dfe9249357bf34aeefbc97a215bc6d8011
mpmcauliffe <mcauliffe99@gmail.com>
2023-07-19 23:49:15 -0400

    FRONTEND: add redux/reducers/ accountReducer & authReducer

---
+++ frontend/src/redux/reducers/accountReducer.js
@@ -0,0 +1,57 @@
+import { GET_USER_INFO, USER_MESSAGE, CLEAR_USER_MESSAGE, } from '../types'
+
+
+const initialState = {
+    displayName: '',
+    image: '',
+    secondaryImage: '',
+    userId: '',
+    userMessage: {
+        message: '',
+        style: '',
+        timeDisplay: null,
+    },
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case GET_USER_INFO:
+            // console.log(action.payload.userInfo)
+            return {
+                ...state,
+                displayName: action.payload.userInfo.displayName,
+                image: action.payload.userInfo.image,
+                userId: action.payload.userInfo.userId,
+                userMessage: {
+                    message: action.payload.message,
+                    style: 'green darken-3 rounded',
+                    timeDisplay: 5000,
+                }
+            }
+
+        case USER_MESSAGE:
+            return {
+                ...state,
+                userMessage: {
+                    message: action.payload,
+                    style: 'red accent-4 rounded',
+                    timeDisplay: 5000,
+                }
+            }
+
+        case CLEAR_USER_MESSAGE:
+            return {
+                ...state,
+                userMessage: {
+                    message: '',
+                    style: '',
+                    timeDisplay: null,
+                },
+            }
+        
+
+        default:
+            return state
+    }
+}
---
+++ frontend/src/redux/reducers/authReducer.js
@@ -0,0 +1,44 @@
+import { LOGIN, LOGOUT, ERROR, TRIGGER_AUTH_ERROR, } from '../types'
+
+
+const initialState = {
+    isAuthenticated: typeof localStorage.token !== 'undefined' ? true : false,
+    error: '',
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case LOGIN:
+            return {
+                ...state,
+                isAuthenticated: true,
+                error: '',
+            }
+
+        case LOGOUT:
+            return {
+                ...state,
+                isAuthenticated: false,
+                error: '',
+            }
+
+        case ERROR:
+            //localStorage.removeItem('token')
+            return {
+                ...state,
+                isAuthenticated: false,
+                error: 'Something went wrong.',
+            }
+
+        case TRIGGER_AUTH_ERROR:
+            return {
+                ...state,
+                isAuthenticated: false,
+                error: action.payload,
+            }
+
+        default:
+            return state
+    }
+}
