ad314057192850aa244ba208def79e76eb3a96bf
mpmcauliffe <mcauliffe99@gmail.com>
2023-07-20 23:29:28 -0400

    FRONTEND: add redux/types

---
+++ frontend/src/redux/types.js
@@ -0,0 +1,46 @@
+/* AUTH TYPES */
+export const LOGIN                 = 'LOGIN'
+export const LOGOUT                = 'LOGOUT'
+export const ERROR                 = 'ERROR'
+export const TRIGGER_AUTH_ERROR    = 'TRIGGER_AUTH_ERROR'
+
+/* BOOK TYPES */
+export const GET_MY_BOOKS                   = 'GET_MY_BOOKS'
+export const SEARCH_BOOKS                   = 'SEARCH_BOOKS'
+export const ADD_BOOK_TO_PROFILE            = 'ADD_BOOK_TO_PROFILE'
+export const REMOVE_BOOK_FROM_LIBRARY       = 'REMOVE_BOOK_FROM_LIBRARY'
+export const GET_CLUB_BOOKS                 = 'GET_CLUB_BOOKS'
+export const ADD_BOOK_TO_CLUB               = 'ADD_BOOK_TO_CLUB'
+export const NO_BOOKS_TO_LOAD               = 'NO_BOOKS_TO_LOAD,'
+export const MESSAGE                        = 'MESSAGE'
+export const BOOK_ERROR                     = 'BOOK_ERROR'
+export const CLEAR_BOOK_MESSAGE             = 'CLEAR_BOOK_MESSAGE'
+
+/* CLUB TYPES */
+export const GET_CLUB               = 'GET_CLUB'
+export const GET_MULTIPLE_CLUBS     = 'GET_MULTIPLE_CLUBS'
+export const GET_MY_CLUBS           = 'GET_MY_CLUBS'
+export const CREATE_CLUB            = 'CREATE_CLUB'
+export const JOIN_CLUB              = 'JOIN_CLUB'
+export const LEAVE_CLUB             = 'LEAVE_CLUB'
+export const SEARCH_CLUBS           = 'SEARCH_CLUB'
+export const CLEAR_SEARCH_CLUBS     = 'CLEAR_SEARCH_CLUBS'
+export const RESET_CLUB             = 'RESET_CLUB'
+export const CLUB_MESSAGE           = 'CLUB_MESSAGE'
+export const CLEAR_CLUB_MESSAGE     = 'CLEAR_CLUB_MESSAGE'
+
+/* COMMENT TYPES */
+export const GET_COMMENTS           = 'GET_COMMENTS'
+export const REFRESH_COMMENTS       = 'REFRESH_COMMENTS'
+export const RESET_COMMENTS         = 'RESET_COMMENTS'
+export const TOGGLE_NEW_INPUT       = 'TOGGLE_NEW_INPUT'
+export const COMMENT_MESSAGE        = 'COMMENT_MESSAGE'
+export const CLEAR_COMMENT_MESSAGE  = 'CLEAR_COMMENT_MESSAGE'
+
+/* ACCOUNT TYPES */
+export const GET_USER_INFO              = 'GET_USER_INFO'
+export const USER_MESSAGE               = 'USER_MESSAGE'
+export const CLEAR_USER_MESSAGE         = 'CLEAR_USER_MESSAGE'
+
+
+export const SET_LOADING      = 'SET_LOADING'
