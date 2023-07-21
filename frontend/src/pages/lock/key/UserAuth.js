aae63e1acec7923e4f55bd92b3c369c2d5814eee
mpmcauliffe <mcauliffe99@gmail.com>
2023-07-21 17:05:01 -0400

    FRONTEND: add pages/lock/key/

---
+++ frontend/src/pages/lock/key/UserAuth.js
@@ -0,0 +1,43 @@
+import React, { useEffect, } from 'react'
+import { useHistory, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { loginUser, logout } from '../../../redux/actions/authActions'
+import { Spinner } from '../../../components'
+
+
+const UserAuth_proto = ({ loginUser, logout, isAuthenticated, }) => {
+    const history = useHistory()
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            console.log('loginUser')
+            setTimeout(() => { loginUser(history) }, 1000)
+        } else {
+            console.log('logout')
+            setTimeout(() => { logout(history) }, 1000)
+        }
+    })
+
+    // if (isAuthenticated) { <Redirect to='/dashboard' /> }
+
+    // if (!isAuthenticated) { <Redirect to='/Landing' /> }
+
+    return <Spinner />
+}
+
+
+UserAuth_proto.propTypes = {
+    loginUser: PropTypes.func,
+    logout: PropTypes.func,
+    history: PropTypes.object.isRequired,
+    isAuthenticated: PropTypes.bool.isRequired,
+}
+
+const mapStateToProps = state => ({
+    // logout: PropTypes.func.isRequired,
+    isAuthenticated: state.auth.isAuthenticated,
+})
+
+const UserAuth = connect(mapStateToProps, { loginUser, logout, })(UserAuth_proto)
+export { UserAuth }
