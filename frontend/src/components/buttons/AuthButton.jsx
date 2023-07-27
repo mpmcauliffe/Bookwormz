334a72a5768a09ff43dfb000c1471a80095dc1eb
mpmcauliffe <mcauliffe99@gmail.com>
2023-07-26 22:13:25 -0400

    FRONTEND: update Button to sytled-components v5+

--- frontend/src/components/buttons/AuthButton.jsx
+++ frontend/src/components/buttons/AuthButton.jsx
@@ -6,8 +6,8 @@ const Button = styled.button`
     width: 100%;
 
     background: transparent;
-    border: ${p => p.isActive ? '.2rem solid rgb(152,34,51)' : '.2rem solid rgb(170,170,170)'};
-    color: ${p => p.isActive ? '#982233' : '#aaa'};
+    border: ${p => p.$isActive ? '.2rem solid rgb(152,34,51)' : '.2rem solid rgb(170,170,170)'};
+    color: ${p => p.$isActive ? '#982233' : '#aaa'};
     font-size: 2rem;
 
     cursor: pointer;
@@ -15,7 +15,7 @@ const Button = styled.button`
 
 export const AuthButton = ({ children, toggle }) => {
     return (
-        <Button isActive={toggle}>
+        <Button $isActive={toggle}>
             {children}
         </Button>
     )
